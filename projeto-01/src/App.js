2383939a112104843653c821478023b69aa1f948
Gusvioli <gustavovieiradeoliveira@gmail.com>
2022-08-13 19:37:28 -0300

    Iniciando projeto 01 do protfóleo de Gustavo Vieira

---
+++ projeto-01/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">{}</header>
+      <main className="App-main">
+        <div className="App-content">{}</div>        
+      </main>
+      <footer className="App-footer">{}</footer>
+    </div>
+  );
+}
+
+export default App;
