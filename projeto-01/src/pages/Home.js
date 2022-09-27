db9d303816e42dd7c0183bb276ac06cd06a3db8c
Gusvioli <gustavovieiradeoliveira@gmail.com>
2022-09-27 08:25:01 -0300

    Update portfóleo

--- projeto-01/src/pages/Home.js
+++ projeto-01/src/pages/Home.js
@@ -7,13 +7,9 @@ function Home() {
     const handleRandDiv = () => {
       const randDiv = {
         width: 100,
-        height: 15,
+        height: 10,
         padding: 10,
-        backgroundColor: `rgb(${[
-          Math.floor(Math.random()  * 205) + 50,
-          Math.floor(Math.random()  * 205) + 50,
-          Math.floor(Math.random()  * 205) + 50,
-        ]})`,
+        backgroundColor: `rgb(${[80,80,120]})`,
       };
       return randDiv;
     }
@@ -21,13 +17,9 @@ function Home() {
       const { projetos } = info;
       if (projetos.length > 0) {
         const randDiv = {
-          width: Math.floor(Math.random()  * 150) + 50,
-          height: Math.floor(Math.random() * 80) + 20,
-          backgroundColor: `rgb(${[
-            Math.floor(Math.random()  * 205) + 50,
-            Math.floor(Math.random()  * 205) + 50,
-            Math.floor(Math.random()  * 205) + 50,
-          ]})`,
+          width: 400,
+          height: 250,
+          backgroundColor: `rgb(${[80,80,120]})`,
         };
         return randDiv;
       } else {
@@ -44,7 +36,7 @@ function Home() {
     useEffect(() => {
         const array = [];
         document.title = `${info.nome}: ${info.title}`;
-        for (let i = 0; i < 462; i++) array.push(i);
+        for (let i = 0; i < 4; i++) array.push(i);
         setArray(array);
     } , [info, setArray, rgb, setRgb]);
 
@@ -67,29 +59,35 @@ function Home() {
                     <div className='menu-header-div-interno'>Sobre mim</div>
                   </div>
                 </a>
+                <a href='#habilidades'>
+                  <div className='menu-header-div' style={ handleRandDiv() }>
+                    <div className='menu-header-div-interno'>Habilidades</div>
+                  </div>
+                </a>
                 <div className='menu-header-div' style={ handleRandDiv() }>
                   <div className='menu-header-div-interno'>Contato</div>
                 </div>
               </section>
-              <section className='menu-header-tecs'>
-                <div className='menu-header-div-tecs' >
-                  <div className='menu-header-div-tecs-name'>Tecnologias aprendidas: </div>
+              <div className="App-central"></div> 
+            </header>
+            <main>
+              <section className="App-main">
+                <h1 id='habilidades'>Habilidades</h1>
+                <section className='menu-header-tecs'>
+                  <div>
                     { info.srcTec.map((ma, index) => 
                     <span>
                       <a key={index} target="_blank" rel="noopener noreferrer nofollow" href={ma}>
-                        <img height="40px"
+                        <img height="70px"
                           src={ma}
                           data-canonical-src="https://www.vectorlogo.zone/logos/eslint/eslint-icon.svg"
                           alt='' />
                       </a>
                     </span>
                     ) }
-                </div>
-              </section>
-            <div className="App-central"></div> 
-            </header>
-            <main>
-              <section className="App-main">
+                  </div>
+                </section>
+                <h1>Projetos</h1>
                 <section className="App-section">
                   { array.map((ma, index) => <div className='App-project' style={ headerDiv() } key={index}>
                     <span className='App-project-opacity'></span>
@@ -118,7 +116,6 @@ function Home() {
                     
                   </span>
                 </div>
-                
             </footer>
         </div>
     );
