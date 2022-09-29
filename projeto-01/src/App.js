1c2a50ecbfd986b7fb9b807ef393088268a97ea2
Gusvioli <gustavovieiradeoliveira@gmail.com>
2022-09-29 10:24:43 -0300

    Update gusvioli portfóleo

--- projeto-01/src/App.js
+++ projeto-01/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
 import Provider from './context/Provider';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
+import Guscrono from './pages/Guscrono';
 
 function App() {
   return (
     <Provider>
-      <Home />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={ Home } />
+          <Route exact path="/guscrono" component={ Guscrono } />
+        </Switch>
+      </BrowserRouter>
     </Provider>
   );
 }
---
+++ projeto-01/src/pages/Guscrono.js
@@ -0,0 +1,10 @@
+import React from "react";
+import '../css/Guscrono.css';
+
+function Guscrono(params) {
+    return(
+        <div>Gustavo</div>
+    );
+}
+
+export default Guscrono;
