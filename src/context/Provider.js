76c6e8d449b89855e7095b0accff358071300dfb
Gusvioli <gustavovieiradeoliveira@gmail.com>
2022-10-09 15:04:35 -0300

    Up

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -23,7 +23,7 @@ function Provider({ children }) {
         'https://www.vectorlogo.zone/logos/mysql/mysql-official.svg',
       ],
       usoSite: [3, 7, 8, 9, 2],
-      sobreMin: 'Sou Gustavo Vieira, nasci no Rio de Janeiro - RJ vivo no bairro da Ilha do Governador, aos 16 anos descobrir um forte interesse por computadores e programação, tenho curiosidade em saber como funciona as coisas, nessa idade comecei aprender HTML estudei por um ano, depois me interessei por CSS e PHP desde então vivo aprendendo linguagens de programação e tudo o que puder sobre computadores, luto pra alcançar meus objetivos; tenho conhecimento em Python, PHP, HTML5, CSS, JAVASCRIPT, REACT, JEST, RTL, GITHUB, NODE JS, REDUX, DOCKER , SQL e Metodologias Ágeis',
+      sobreMin: 'Sou Gustavo Vieira, nasci no Rio de Janeiro - RJ vivo no bairro da Ilha do Governador, aos 16 anos descobri um forte interesse por computadores e programação, tenho curiosidade em saber como funciona as coisas, nessa idade comecei aprender HTML estudei por um ano, depois me interessei por CSS e PHP desde então vivo aprendendo linguagens de programação e tudo o que puder sobre computadores, luto pra alcançar meus objetivos; tenho conhecimento em Python, PHP, HTML5, CSS, JAVASCRIPT, REACT, JEST, RTL, GITHUB, NODE JS, REDUX, DOCKER , SQL e Metodologias Ágeis',
       projetosSrc: [
         {
           nome: 'Tryunfo',
